830f2d8c0a8692e55861dbbc7887e32dc5329f96
MIchael Love <Michael.t.love3@gmail.com>
2024-04-19 22:13:43 -0500

    Implemented further logic in the menu system. Now properly is able to navigate back and forth. Properly updates in the background with resources and has proper logic for building structures.

--- Castle/Castle/src/App.java
+++ Castle/Castle/src/App.java
@@ -4,6 +4,8 @@ import java.util.concurrent.Executors;
  
 
 public class App {
+    
+    public static Castle myCastle = new Castle("Myke", 0,0,0);
     public static void main(String[] args) throws Exception {
         // Purpose is to have a castle object that generates resources over time (Async thread)
         // Use resources to schedule tasks that utilize them (Task Manager / Async)
@@ -11,30 +13,22 @@ public class App {
         // Clean up Screen 
         ClearScreen();
 
+        // Create the Main Menu
+
         // Create a Executor Service that will run threads asynchronously (increase/decrease pool as needed)
-        ExecutorService threadRunner = Executors.newFixedThreadPool(1);
+        ExecutorService threadRunner = Executors.newFixedThreadPool(2);
 
         // Create the Castle
-        Castle myCastle = new Castle("Myke", 0);
 
         // Add the generateResources() as an Async Thread (runs in background)
         threadRunner.submit(myCastle::generateResources);
+        threadRunner.submit(myCastle::mainMenu);
 
-        // Displays menu, May change from displayOptions() -> Menu()
-        myCastle.displayOptions();
        
-            
-        
-            
-
-
-
-        
-
 
 
         // Shudown threadRunner when no longer in use
-        threadRunner.shutdown();
+        // threadRunner.shutdown();
 
     }
 
--- Castle/Castle/src/Castle.java
+++ Castle/Castle/src/Castle.java
@@ -1,73 +1,195 @@
 import java.util.Scanner;
 
-public class Castle {
+public class Castle extends Thread {
 
-    private String name;
-    private int wood;
-    public int userChoice;
+    
+    public String name;
+    public int wood;
+    public int woodPerMinute;
+    public int stone;
+    public int stonePerMinute;
+    public int gold;
+    public int goldPerMinute;
+    public int populationTotal;
+    public int userChoice = 99;
+
+
+    public int houseCost = 50;
+    public int houseTotal;
 
     // Scanner for input
-    Scanner input = new Scanner(System.in);
-    
-    public Castle(String name, int wood)
+    public Scanner input = new Scanner(System.in);
+
+   
+
+    public Castle(String name, int wood, int stone, int gold)
     {
         this.name = name;
         this.wood = wood;
-        this.userChoice = 0;
+        this.woodPerMinute = 60;
+        this.gold = gold;
+        this.stonePerMinute = 60;
+        this.stone = stone;
+        this.goldPerMinute = 60;
+
+        
+
         
     }
 
     public void generateResources()
     {
         while (true) {
-            this.wood++;
+            this.wood += (woodPerMinute / 60);
+            this.stone += (woodPerMinute / 60);
+            this.gold += (woodPerMinute / 60);
             try {
                 // 1 wood per sec
                 Thread.sleep(1000);
             } catch (Exception e) {
-                // TODO: handle exception
             }
-        }
-    }
-
-    public int getUserChoice()
-    {
-        System.out.println("\nChoice >>: ");
-        this.userChoice = input.nextInt();
-        return this.userChoice;
-    }
-
-    public void displayOptions()
-    {
-        ClearScreen();
-        System.out.println(String.join("\n", "Castle "+this.name));
-        System.out.println(String.join("\n", "\n=== Choices ===", "1.) View Resources"));
-        this.userChoice = input.nextInt();
-        switch (this.userChoice) {
-        case 1:
-            this.displayResources();
-            break;
-        
-        case 2:
-            System.exit(1);
-
-    
-        default:
-            break;  
         }
     }
 
     public void displayResources()
     {
-        while (this.userChoice != 99) {
-            ClearScreen();
-            System.out.println("Wood: "+this.wood);
-            System.out.println("\n=== Press '99' to go back to menu");
-            this.userChoice = input.nextInt(); 
-            
-        }
-        this.displayOptions();
+        // Clean up the screen
+        ClearScreen();
+        // Display the resources
+        System.out.println("Wood: "+this.wood);
+        System.out.println("Wood: "+this.stone);
+        System.out.println("Wood: "+this.gold);
+
+        // Display the structures
+        System.out.println("\nHouses: "+this.houseTotal);
+
+        input.nextLine();
+        mainMenu();
+
     }
+
+    public void buildHouse()
+    {
+        int buildChoice;
+
+        // Add in confirmation later
+        if (this.wood >= houseCost) {
+            // If wood stock is greater than house cost then allow player to build
+            this.houseTotal++;
+            this.populationTotal++;
+            this.wood = this.wood - houseCost;
+            System.out.println("\n - A new house was built in the kingdom! -");
+            System.out.println("\n0.) BACK");
+            // input.nextLine();
+            buildChoice = input.nextInt();
+            switch (buildChoice) {
+                case 0:
+                    buildMenu();
+                    break;
+            
+                default:
+                break;
+            }
+        }
+            // TODO: This option has a problem when less than 50 wood with the program hanging.
+            // If wood is less than house cost, don't allow player to build any houses
+            System.out.println("=== You do not have enought WOOD to build this ===");
+            System.out.println("\n0.) BACK");
+            buildChoice = input.nextInt();
+            switch (buildChoice) {
+                case 0:
+                    this.buildMenu();
+                    break;
+            
+                default:
+                    break;
+            }
+
+    }
+
+    public void mainMenu()
+    {
+        ClearScreen();
+        System.out.println(String.join("\n", "Castle "+this.name));
+        System.out.println("\n- Generating: ");
+        System.out.println("--Wood: "+this.woodPerMinute+" /per minute");
+        System.out.println("--Stone: "+this.stonePerMinute+" /per minute");
+        System.out.println("--Gold: "+this.goldPerMinute+" /per minute");
+
+        System.out.println(String.join("\n", "\n=== Choices ===", "1.) View Resources", "2.) Build Structure", "0.) EXIT"));
+        this.getUserChoice();
+        switch (this.userChoice) {
+            case 1:
+                this.displayResources();
+            case 2:
+                this.buildMenu();
+        
+            default:
+                this.mainMenu();;
+        }
+            
+    }
+    
+    public void buildMenu()
+    {
+        ClearScreen();
+        System.out.println(String.join("\n", "=== Build Menu ===", "1.) House ("+this.houseCost+" WOOD)", "0.) BACK"));
+        int buildChoice = this.input.nextInt();
+
+        switch (buildChoice) {
+            case 1:
+                this.buildHouse();
+                break;
+
+            case 0:
+            mainMenu();
+            break;
+        
+            default:
+                mainMenu();
+        }
+    }
+
+    public int getUserChoice()
+        {
+            System.out.println("\nChoice >>: ");
+            this.userChoice = this.input.nextInt();
+            this.input.nextLine();
+            return this.userChoice;
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
     public static void ClearScreen()    {
