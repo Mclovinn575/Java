1eebabdec05b4f7a274e4fb9d00f97c4c3d45e04
Michael Love <Michael.Love@fcserv.com>
2024-05-01 08:06:42 -0500

    Attempting to pull from main

--- ASCII/src/App.java
+++ ASCII/src/App.java
@@ -14,18 +14,11 @@ public class App {
         // X->X
         //    |
         // X<-X
+
         Room room1 = new Room(Room.West, Room.East);
-        room1.ExamineRoom();
-        System.console().readLine();
         Room room2 = new Room(Room.West, Room.South);
-        room2.DisplayRoom();
-        System.console().readLine();
         Room room3 = new Room(Room.North, Room.West);
-        room3.DisplayRoom();
-        System.console().readLine();
         Room room4 = new Room(Room.South, null);
-        room4.DisplayRoom();
-        System.console().readLine();
 
 
         
